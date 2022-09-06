10639f81483401f9571320cdd72d29316169f857
HichamOujari <hichamoujari99@gmail.com>
2022-09-06 08:55:55 +0100

    dsfdsf

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -50,38 +50,40 @@ export default function Home() {
         <div className={styles.conditions}>
           {listSelectedFields.map((ele, index) => (<div key={index} className={styles.condition}>
             <p className={styles.title}>{index == 0 ? 'WHERE' : 'AND'}</p>
-            <select  onChange={(e) => {
+            <select onChange={(e) => {
               const value = e.target.value;
               const newTmp = listFields.filter(spele => spele.name === value)
-              setlistSelectedFields([...newTmp,...listSelectedFields.filter(spele => spele.name!=ele.name)])
+              setlistSelectedFields([...newTmp, ...listSelectedFields.filter(spele => spele.name != ele.name)])
               setListFields([ele, ...listFields.filter(field => field.name != value)])
             }}>
               <option value={ele.name} hidden={true}>{ele.name}</option>
-              {listFields.map((s_ele,index) => <option key={index} value={s_ele.name}>{s_ele.name}</option>)}
+              {listFields.map((s_ele, index) => <option key={index} value={s_ele.name}>{s_ele.name}</option>)}
             </select>
             <select>
-              {ele.operations.map((sp_ele,index) => (<option key={index} value={sp_ele}>{sp_ele}</option>))}
+              {ele.operations.map((sp_ele, index) => (<option key={index} value={sp_ele}>{sp_ele}</option>))}
             </select>
             {
               ele.propositions ? <select>
-                {ele.propositions.map((sp_ele,index) => (<option key={index} value={sp_ele}>{sp_ele}</option>))}
+                {ele.propositions.map((sp_ele, index) => (<option key={index} value={sp_ele}>{sp_ele}</option>))}
               </select> : <input type="text" placeholder="value" />
             }
-            <p className={styles.removeClause} onClick={()=>{
+            <p className={styles.removeClause} onClick={() => {
               setlistSelectedFields([...listSelectedFields.filter(spele => spele.name != ele.name)])
-              setListFields([ele,...listFields])
+              setListFields([ele, ...listFields])
             }}>x</p>
           </div>))}
         </div>
-        {listFields.length != 0 ? <button onClick={() => {
-          const tmp = listFields[0];
-          setlistSelectedFields([...listSelectedFields,tmp])
-          setListFields([...listFields.filter(sp_ele => sp_ele.name != tmp.name)]);
-        }}>Add clause</button> : ''}
-        <button onClick={() => {
-          console.log('listSelectedFields : ',listSelectedFields)
-          console.log('listFields : ',listFields)
-        }}>show</button>
+        <div className={styles.buttons}>
+          {listFields.length != 0 ? <button onClick={() => {
+            const tmp = listFields[0];
+            setlistSelectedFields([...listSelectedFields, tmp])
+            setListFields([...listFields.filter(sp_ele => sp_ele.name != tmp.name)]);
+          }}>Add clause</button> : ''}
+          <button onClick={() => {
+
+          }}>Filter</button>
+        </div>
+
       </main>
     </div>
   )
